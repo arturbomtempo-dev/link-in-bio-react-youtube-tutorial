0ae7f278e494405b6debb17cb4859d7f5ae47ce2
Artur Bomtempo Colen <arturbcolen@gmail.com>
2025-07-17 00:19:22 -0300

    refactor: update links and descriptions for clarity and accuracy

--- src/data/links.ts
+++ src/data/links.ts
@@ -5,21 +5,27 @@ export interface LinkItem {
 }
 
 export const links: LinkItem[] = [
-    {
-        id: 1,
-        link: 'https://www.youtube.com/@ArturBomtempoDev',
-        text: 'Acompanhe o canal do YouTube',
-    },
+    { id: 1, link: 'https://arturbomtempo.dev/', text: 'Portfólio' },
     {
         id: 2,
-        link: 'https://discord.gg/6gbYPRKx',
-        text: 'Faça parte da nossa comunidade do Discord',
+        link: 'https://github.com/arturbomtempo-dev?tab=overview&from=2025-07-01&to=2025-07-16',
+        text: 'GitHub',
     },
-    { id: 3, link: 'https://arturbomtempo.dev/', text: 'Acesse meu portfólio' },
-    { id: 4, link: 'mailto:arturbcolen@gmail.com', text: 'Entre em contato comigo por e-mail' },
+    { id: 3, link: 'https://www.linkedin.com/in/artur-bomtempo/', text: 'Linkedin' },
+    {
+        id: 4,
+        link: 'https://www.youtube.com/@ArturBomtempoDev',
+        text: 'Canal do YouTube',
+    },
     {
         id: 5,
-        link: 'https://open.spotify.com/user/ogddurx9hwlbmrom6wokt87je?si=95ba43077b9a42c3',
-        text: 'Ouça minhas playlists no Spotify',
+        link: 'https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqa0U0VHBLeUpCYVR1TmRsdS1SakN5Z0pWTE1Vd3xBQ3Jtc0tuaUh4RDdaeThpTUtYVTk1QmxHaWttUzlNUFZ0Sk5BYmtUbFBKQ0dTN2hBbmg0NVFuNEx4ZkhEYTZrcFQxYmZlUFUxZ0FVR25KaktRaGU0WDlteTNGcklXOTE5UFg4bHQ2bmZoV0V4M3NXUEs5QzNHQQ&q=https%3A%2F%2Fdiscord.gg%2FPerNehkHE3&v=f_1wsIr-hQw',
+        text: 'Comunidade do Discord',
     },
+    { id: 6, link: 'https://www.instagram.com/arturbomtempo.dev/', text: 'Instagram' },
+    { id: 7, link: 'https://medium.com/@arturbomtempo', text: 'Artigos no Medium' },
+    { id: 8, link: 'https://materials.arturbomtempo.dev/', text: 'Meus Materiais' },
+    { id: 9, link: 'https://www.npmjs.com/~arturbomtempo', text: 'Perfil no NPM' },
+    { id: 10, link: 'mailto:arturbcolen@gmail.com', text: 'E-mail' },
+    { id: 11, link: 'mailto:https://orcid.org/0009-0006-2046-455X', text: 'OrcID' },
 ];
